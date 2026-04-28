ff766a6fae79d04b6f5592c29e36afaf98d07224
sakeh <rifkisyekhi20@gmail.com>
2026-04-28 12:00:45 +0700

    buat admin panel untuk atur barang 4

--- models/Barang.js
+++ models/Barang.js
@@ -3,14 +3,10 @@ const mongoose = require('mongoose');
 const BarangSchema = new mongoose.Schema({
     id_barang: { type: String, required: true, unique: true },
     nama: { type: String, required: true },
-    kategori: { 
-        type: String, 
-        required: true,
-        enum: ['ATK', 'Elektronik'] // <-- KUNCI DI SINI
-    },
+    kategori: { type: String, default: 'ATK' },
     stok: { type: Number, required: true, default: 0 },
     satuan: { type: String, default: 'Pcs' },
     img: { type: String, default: '' }
 });
 
-module.exports = mongoose.model('Barang', BarangSchema);
+module.exports = mongoose.model('Barang', BarangSchema, 'barang');
