6fc960fa24ee6d4f0645819594fe6ef3c83198e1
sakeh <rifkisyekhi20@gmail.com>
2026-04-29 14:02:27 +0700

    tambah konversi gambar

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ const Barang = require('./models/Barang');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -1702,7 +1704,7 @@ client.on("message", async (message) => {
             if (peg && peg["Nama Pegawai"]) namaPeminjam = peg["Nama Pegawai"];
 
             text += `\n~- ${m.nama} (${m.plat})~`;
-            text += `\n   └ Dipakai: ${namaPeminjam}`;
+            text += `\n  └ Dipakai: ${namaPeminjam}`;
           });
         } else {
           text += `\n_(Tidak ada ${jenisFilter.toLowerCase()} yang sedang keluar)_`;
